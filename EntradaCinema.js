2d1efb54c18c4a49ce0997cb8aea050a3e5286b4
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 20:46:35 -0300

    Update EntradaCinema.js

--- EntradaCinema.js
+++ EntradaCinema.js
@@ -1,19 +1,30 @@
-const idade = 15;
+//condicionais if - else
 
+//menores de 18 --> Menor de idade (meia)
+//Entre 18 e 60 anos -> Adulto(meia apenas com caaarteirinha)
+//maiores de 60 anos - > idoso(meia)
+
+
+const idade = 35;
 const temCarteirinha = false;
 
 if(idade<18){
-    //menor de idade
-    console.log("Meia");
-} else if(idade <=60){
-    //adulta
-    if(temCarteirinha ===true){
-        //adulta tem carteirinha
-        console.log("MEIA")
-    } else{
-        console.log('Inteira')
-    }
-} else{
-    //idosa
-    console.log("Meia");
+  //menor de idade
+  console.log("MEIA")
+
+}else if(idade<=60){
+
+  if(temCarteirinha==true){
+    //adulto com carteirinha
+    console.log("MEIA")
+
+  }else{
+    console.log("INTEIRA")
+  }
+  
+  
+}else{
+  //idosa
+  console.log("MEIA")
+
 }
