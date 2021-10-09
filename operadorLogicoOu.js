be9a4dfafd3893724f1e1b219006351dd8ad6b43
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 21:21:56 -0300

    Update operadorLogicoOu.js

--- operadorLogicoOu.js
+++ operadorLogicoOu.js
@@ -4,8 +4,19 @@
 //Entre 18 e 60 anos -> Adulto(meia apenas com caaarteirinha)
 //maiores de 60 anos - > idoso(meia)
 
-const idade = 15;
+//MEIA = (menor de idade) ou (idosa) ou (adulta e tiver carteirinha)
+//INTEIRA = O resto.
+
+const idade = 60;
 const temCarteirinha = true;
 
 const ehAdulto = idade >= 18 && idade <= 60;
+
+if (idade < 18 || idade > 60 || ehAdulto && temCarteirinha) {
+  //Nesse caso nao coloquei a exclamação pois está diretamente relacionado com ter ou nao carteirinha.
+  console.log("MEIA")
+
+} else {
+  console.log("INTEIRA")
+}
 
