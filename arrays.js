7925ec77824c83d5e385e106ca42eed0bbfdadf5
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 22:41:25 -0300

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -7,3 +7,17 @@ console.log(listaDeNomes)
 listaDeNomes[5] = "Matilda"
 listaDeNomes[1] = "Severina"
 console.log(listaDeNomes)
+
+//-----------------------------------------------------------
+console.log("")
+
+const notas = []
+//PUSH acrescenta um valor ao array
+notas.push(10);
+notas.push(9);
+notas.push(10);
+//POP remove um valor do array
+notas.pop()
+notas.pop()
+
+console.log(notas)
