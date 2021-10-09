c8016e285c3ae7969ce8eda9d8d9e0d6abb97d83
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 22:00:31 -0300

    Create meSegueNoInsta.js

---
+++ meSegueNoInsta.js
@@ -0,0 +1,10 @@
+const altura = 7
+const largura = 9
+
+if (largura > altura) {
+  console.log("PAISAGEM")
+} else if (altura > largura) {
+  console.log("RETRATO")
+} else {
+  console.log("QUADRADO")
+}
