3bd9d41957e8d30609a3a67ec772fae17b42650e
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 22:36:50 -0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,9 @@
+//Arrays
+
+const listaDeNomes = ["jose", "Ana", "Maria", "Pedro", "Mauricio"];
+
+console.log(listaDeNomes)
+
+listaDeNomes[5] = "Matilda"
+listaDeNomes[1] = "Severina"
+console.log(listaDeNomes)
