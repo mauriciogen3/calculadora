25b5ea44824c9d0d8db90fb9fe0cbb7dab3df756
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-08 21:11:18 -0300

    Create operadorLogicoOu.js

---
+++ operadorLogicoOu.js
@@ -0,0 +1,11 @@
+//condicionais if - else
+
+//menores de 18 --> Menor de idade (meia)
+//Entre 18 e 60 anos -> Adulto(meia apenas com caaarteirinha)
+//maiores de 60 anos - > idoso(meia)
+
+const idade = 15;
+const temCarteirinha = true;
+
+const ehAdulto = idade >= 18 && idade <= 60;
+
