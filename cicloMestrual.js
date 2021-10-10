dcc635f17b3063a23b5be6fa35f94d3d21616122
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-10 14:52:14 -0300

    Update cicloMestrual.js

---
+++ cicloMestrual.js
@@ -0,0 +1,12 @@
+var diaDoCiclo = 15
+
+
+if (diaDoCiclo === 1 || diaDoCiclo === 2 || diaDoCiclo === 3 || diaDoCiclo === 4 || diaDoCiclo === 5) {
+  console.log("MENSTRUACAO")
+} else if (diaDoCiclo === 6 || diaDoCiclo === 7 | diaDoCiclo === 8 || diaDoCiclo === 9 || diaDoCiclo === 10 || diaDoCiclo === 11) {
+  console.log("FASE OVULAR")
+} else if (diaDoCiclo === 12 || diaDoCiclo === 13 | diaDoCiclo === 14 || diaDoCiclo === 15 || diaDoCiclo === 16 || diaDoCiclo === 11) {
+  console.log("OVULACAO")
+} else if (diaDoCiclo === 17 || diaDoCiclo === 18 | diaDoCiclo === 19 || diaDoCiclo === 20 || diaDoCiclo === 21 || diaDoCiclo === 22 || diaDoCiclo === 23 || diaDoCiclo === 24 | diaDoCiclo === 25 || diaDoCiclo === 26 || diaDoCiclo === 27 || diaDoCiclo === 28) {
+  console.log("FASE LUTEA")
+}
