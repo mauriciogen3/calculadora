0dfe7296ca303fa20906678be2c4a3df232d4bdf
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-10 18:31:09 -0300

    Update certificado.js

--- certificado.js
+++ certificado.js
@@ -1,21 +1,36 @@
-var presenca = 100;
+var presenca = 79;
 var entregouTodosOsTrabalhos = true;
-var notaSoftSkills = 90;
-var notaProgramacao = 100;
+var notaSoftSkills = 75;
+var notaProgramacao = 88;
 
-if (presenca >= 75 && entregouTodosOsTrabalhos === true && notaSoftSkills >= 75 && notaProgramacao >= 75) {
+if
+
+  (presenca >= 75 && presenca < 90 &&
+  entregouTodosOsTrabalhos === true &&
+  notaSoftSkills >= 70 && notaSoftSkills <= 99 &&
+  notaProgramacao >= 70 && notaProgramacao <= 99) {
 
   console.log("APROVADA")
 
-} else if (presenca <= 75 && entregouTodosOsTrabalhos == true && notaSoftSkills >= 90 && notaProgramacao <= 75) {
+} else if
+
+  (presenca >= 75 &&
+  entregouTodosOsTrabalhos == true ||
+  notaSoftSkills >= 90 && notaSoftSkills < 100 ||
+  notaProgramacao >= 70) {
 
   console.log("APROVADA - MERITO EM SOFT SKILLS")
 
-} else if (presenca < 75 && entregouTodosOsTrabalhos == true && notaSoftSkills <= 75 && notaProgramacao <= 90) {
+} else if (presenca >= 75 &&
+  entregouTodosOsTrabalhos == true &&
+  notaSoftSkills >= 70 &&
+  notaProgramacao >= 90 && notaProgramacao < 100) {
 
   console.log("APROVADA - MERITO EM PROGRAMACAO")
 
-} else if (presenca == 100 && entregouTodosOsTrabalhos == true && notaSoftSkills == 100 && notaProgramacao == 100) {
+} else if (presenca == 100 &&
+  entregouTodosOsTrabalhos == true &&
+  notaSoftSkills == 100 && notaProgramacao == 100) {
 
   console.log("APROVADA - NOTA 1000")
 
