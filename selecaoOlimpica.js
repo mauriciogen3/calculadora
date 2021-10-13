4776bafa5d3e9cbcec33744946135c0781fe9435
Maurício Junior <mauriciogen3@hotmail.com>
2021-10-13 15:01:37 -0300

    selecaoOlimpica

---
+++ selecaoOlimpica.js
@@ -0,0 +1,25 @@
+const idadesDeUmTime = [ 
+    20,
+    24,
+    21,
+    20,
+    20,
+    17,
+    21,
+    20,
+    22,
+    21,
+    17,
+    17,
+    20,
+    24,
+    17,
+    22,
+    23,
+    24]
+
+let idade = 0
+
+idadesDeUmTime.length
+
+concole.log(idadesDeUmTime)
