fbcb1a671a6d40c91dfddb80270dd0d628829527
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-13 21:38:13 -0300

    Update selecaoOlimpica.js

--- selecaoOlimpica.js
+++ selecaoOlimpica.js
@@ -1,17 +1,17 @@
 const idadesDeUmTime = [20, 24, 21, 20, 20, 22, 21, 17, 17, 20, 20, 17, 22, 23, 24, 22, 23, 24]
 
 
-let idadesDeUmTime1 = 0;
+let maiorQue23 = 0;
 
 for (let item of idadesDeUmTime) {
     if (item > 23) {
-        idadesDeUmTime1++;
+        maiorQue23++;
     }
 }
 
 if (idadesDeUmTime.length === 18) {
     console.log("CONVOCACAO VALIDA")
-    console.log(idadesDeUmTime1)
+    console.log(maiorQue23)
 } else if (idadesDeUmTime.length < 18) {
     console.log("INVALIDA POR TER MENOS DE 18 JOGADORES")
 } else {
