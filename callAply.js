f58333d96787f762532d01eaea0638b375c39747
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-10-29 21:33:31 -0300

    Update callAply.js

--- callAply.js
+++ callAply.js
@@ -8,13 +8,16 @@ const produto = {
   desc: 0.15,
   getPreco
 }
+
 globalThis.preco = 20
 globalThis.desc = 0.1
 console.log(getPreco())
 console.log(produto.getPreco())
 
 const carro = { preco: 49990, desc: 0.2 }
+
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-console.log(getpreco.call(carro, 0.17, '$'))
+console.log(getPreco.call(carro, 0.17, '$'))
+console.log(getPreco.apply(global, [0.17, '$']))
