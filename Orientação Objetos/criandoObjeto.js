be4d21ddb7641910b21f495a4ca809c1abda7223
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-05 22:43:30 -0300

    Update criandoObjeto.js

--- Orientação Objetos/criandoObjeto.js
+++ Orientação Objetos/criandoObjeto.js
@@ -20,3 +20,18 @@ const p1 = new Produto('caneta', 7.99, 0.15)
 const p2 = new Produto('notebook', 2998.99, 0.25)
 
 console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+function criarFuncionario(nome, salarioBase, faltas) {
+  return {
+    nome,
+    salarioBase,
+    faltas,
+    getSalario() {
+      return (salarioBase / 30) * (30 - faltas)
+    }
+  }
+}
+const f1 = criarFuncionario('joao', 7980, 4)
+const f2 = criarFuncionario('Maria', 7980, 1)
+
+console.log(f1.getSalario(), f2.getSalario())
