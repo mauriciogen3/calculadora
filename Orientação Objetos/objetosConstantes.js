6a14223593d9cd05fa9d29f609cd2e8ad360e64b
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-05 23:18:21 -0300

    Create objetosConstantes.js

---
+++ Orientação Objetos/objetosConstantes.js
@@ -0,0 +1,16 @@
+const pessoa = { nome: 'joao' }
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'maria'
+pessoa.end = 'Rua ABC'
+delete pessoa.nome
+
+console.log(pessoa.nome)
+console.log(pessoa)
+
+const pessoaConstante = Object.freeze({ nome: 'Joao' })
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
