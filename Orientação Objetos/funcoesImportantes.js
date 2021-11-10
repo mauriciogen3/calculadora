2c3c7486a5a68c282bb4a001c96972df4ea50e21
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-10 20:23:19 -0300

    Create funcoesImportantes.js

---
+++ Orientação Objetos/funcoesImportantes.js
@@ -0,0 +1,10 @@
+const pessoa = {
+  nome: 'Mauricio',
+  idade: 31,
+  peso: 87
+}
+
+console.log(Object.keys(pessoa))
+//Object é uma coleção dinamica de chaves e valores
+
+console.log(Object.values(pessoa))
