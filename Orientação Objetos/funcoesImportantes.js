5c83b92ac706db31f807c0c21b12655d58992a75
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-10 20:24:47 -0300

    Update funcoesImportantes.js

--- Orientação Objetos/funcoesImportantes.js
+++ Orientação Objetos/funcoesImportantes.js
@@ -8,3 +8,6 @@ console.log(Object.keys(pessoa))
 //Object é uma coleção dinamica de chaves e valores
 
 console.log(Object.values(pessoa))
+//Printa na tela os valores do Objeto criado
+
+console.log(Object.entries(pessoa))
