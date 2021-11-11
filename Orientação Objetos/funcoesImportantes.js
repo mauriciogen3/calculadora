8e08a2062009143b9e1a428541ce020332a0cfdd
Mauriciogen3 <mauriciogen3@hotmail.com>
2021-11-10 21:10:17 -0300

    Update funcoesImportantes.js

--- Orientação Objetos/funcoesImportantes.js
+++ Orientação Objetos/funcoesImportantes.js
@@ -11,3 +11,20 @@ console.log(Object.values(pessoa))
 //Printa na tela os valores do Objeto criado
 
 console.log(Object.entries(pessoa))
+//printa  o array com todos os atributos (chave e valores)
+
+Object.entries(pessoa).forEach(([chave, valor]) => {
+  console.log(`${chave}: ${valor}`)
+})
+
+Object.defineProperty(pessoa, 'dataNascimento', {
+  enumerable: true,
+  writable: false,
+  value: '01/01/2019'
+})
+//inserir outro valor a chave pessoa
+
+pessoa.dataNascimento = '01/01/2017'
+//writable: false nao permite que o valor seja alterado.
+console.log(pessoa.dataNascimento)
+console.log(Object.keys(pessoa))
